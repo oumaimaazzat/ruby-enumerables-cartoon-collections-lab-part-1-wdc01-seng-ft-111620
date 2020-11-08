81270483bc792f1715c6fd83307ab1a0f68f67a1
Oumaima Azzat <oumaimazzat@gmail.com>
2020-11-08 20:15:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,5 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
+  dwarves_array = ["Gimli", "Thorin", "Balin", "Gloin"]
 end
